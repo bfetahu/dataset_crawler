5a2874df36247894ee3178fc6f2022c5e4138834
Besnik Fetahu <fetahu@l3s.de>
2014-08-11 16:16:07 +0200

    storing the resource updates in a batch rather than individually.

--- ld_crawler_schema.sql
+++ ld_crawler_schema.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `ld_crawler` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `ld_crawler`;
+CREATE DATABASE  IF NOT EXISTS `ld_dataset_crawler` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `ld_dataset_crawler`;
 -- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
 --
 -- Host: db.l3s.uni-hannover.de    Database: ld_dataset_crawler
@@ -33,7 +33,7 @@ CREATE TABLE `crawl_log` (
   `crawl_description` text,
   `crawl_date` timestamp NOT NULL default '0000-00-00 00:00:00',
   PRIMARY KEY  (`crawl_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -57,7 +57,7 @@ CREATE TABLE `crawl_operations_log` (
   KEY `log_date_idx` (`log_date`),
   KEY `log_method_idx` (`log_method`(255)),
   KEY `crawl_idx` (`crawl_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5416173 DEFAULT CHARSET=utf8 COMMENT='InnoDB free: 7168 kB';
+) ENGINE=InnoDB AUTO_INCREMENT=2047875 DEFAULT CHARSET=utf8 COMMENT='InnoDB free: 7168 kB';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,16 +70,11 @@ DROP TABLE IF EXISTS `crawl_setups`;
 CREATE TABLE `crawl_setups` (
   `setup_id` int(10) unsigned NOT NULL auto_increment,
   `datahub_keywords` text NOT NULL,
-  `mysql_host` varchar(500) NOT NULL default '',
-  `mysql_schema` varchar(500) NOT NULL default '',
-  `mysql_user` varchar(500) NOT NULL default '',
-  `mysql_pwd` varchar(500) NOT NULL default '',
   `crawl_description` varchar(45) default NULL,
-  `is_datahub_group_search` tinyint(1) NOT NULL default '0',
   `is_completed` tinyint(1) default NULL,
   `user_id` int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (`setup_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='InnoDB free: 8192 kB';
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='InnoDB free: 8192 kB; InnoDB free: 4096 kB; InnoDB free: 112';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -97,7 +92,7 @@ CREATE TABLE `dataset` (
   `dataset_id_datahub` varchar(200) NOT NULL,
   PRIMARY KEY  (`dataset_id`),
   UNIQUE KEY `dataset_id_datahub_UNIQUE` (`dataset_id_datahub`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -132,7 +127,7 @@ CREATE TABLE `dataset_crawl_log` (
   `timestamp_start` timestamp NOT NULL default '0000-00-00 00:00:00',
   `timestamp_end` timestamp NOT NULL default '0000-00-00 00:00:00',
   PRIMARY KEY  (`crawl_id`,`dataset_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT='InnoDB free: 9216 kB; InnoDB free: 9216 kB';
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COMMENT='InnoDB free: 9216 kB; InnoDB free: 9216 kB';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -147,7 +142,7 @@ CREATE TABLE `dataset_namespaces` (
   `namespace_id` int(11) NOT NULL,
   KEY `fk_dataset_id_idx` (`dataset_id`),
   KEY `fk_schema_id_idx` (`namespace_id`),
-  CONSTRAINT `fk_schema_dataset_schemas` FOREIGN KEY (`namespace_id`) REFERENCES `namespaces` (`namespace_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_schema_dataset_schemas` FOREIGN KEY (`namespace_id`) REFERENCES `namespace` (`namespace_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_dataset_dataset_schemas` FOREIGN KEY (`dataset_id`) REFERENCES `dataset` (`dataset_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -170,7 +165,7 @@ CREATE TABLE `dataset_namespaces_log` (
   KEY `fk_crawl_idx` (`crawl_id`),
   KEY `log_type_idx` (`log_type`),
   KEY `crawl_idx` (`crawl_id`),
-  CONSTRAINT `fk_schema_dataset_schema_log` FOREIGN KEY (`namespace_id`) REFERENCES `namespaces` (`namespace_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_schema_dataset_schema_log` FOREIGN KEY (`namespace_id`) REFERENCES `namespace` (`namespace_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_crawl_dataset_schema_log` FOREIGN KEY (`crawl_id`) REFERENCES `crawl_log` (`crawl_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_dataset_dataset_schema_log` FOREIGN KEY (`dataset_id`) REFERENCES `dataset` (`dataset_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='InnoDB free: 21504 kB; (`crawl_id`) REFER `ld_dataset_crawle';
@@ -180,16 +175,16 @@ CREATE TABLE `dataset_namespaces_log` (
 -- Table structure for table `namespace`
 --
 
-DROP TABLE IF EXISTS `namespaces`;
+DROP TABLE IF EXISTS `namespace`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `namespaces` (
+CREATE TABLE `namespace` (
   `namespace_id` int(11) NOT NULL auto_increment,
   `namespace_uri` varchar(200) NOT NULL,
   PRIMARY KEY  (`namespace_id`),
-  UNIQUE KEY `namespace_id_UNIQUE` (`namespace_id`),
-  UNIQUE KEY `namespace_uri_UNIQUE` (`namespace_uri`)
-) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8;
+  UNIQUE KEY `schema_id_UNIQUE` (`namespace_id`),
+  UNIQUE KEY `schema_uri_UNIQUE` (`namespace_uri`)
+) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -209,7 +204,7 @@ CREATE TABLE `namespaces_instance_log` (
   KEY `fk_schema_instance_idx_schema_log` (`namespace_value_uri`),
   KEY `fk_crawl_idx_schema_log` (`crawl_id`),
   KEY `fk_dataset_idx_schema_log` (`dataset_id`),
-  CONSTRAINT `fk_schema_schema_log` FOREIGN KEY (`namespace_id`) REFERENCES `namespaces` (`namespace_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_schema_schema_log` FOREIGN KEY (`namespace_id`) REFERENCES `namespace` (`namespace_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_crawl_schema_log` FOREIGN KEY (`crawl_id`) REFERENCES `crawl_log` (`crawl_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_dataset_schema_log` FOREIGN KEY (`dataset_id`) REFERENCES `dataset` (`dataset_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -228,9 +223,8 @@ CREATE TABLE `namespaces_instances` (
   `namespace_value_type` binary(1) NOT NULL,
   `namespace_value_description` varchar(500) default NULL,
   PRIMARY KEY  (`namespace_value_uri`,`namespace_id`),
-  UNIQUE KEY `schema_id_UNIQUE` (`namespace_id`),
-  UNIQUE KEY `schema_value_UNIQUE` (`namespace_value_uri`),
-  CONSTRAINT `fk_schema_id` FOREIGN KEY (`namespace_id`) REFERENCES `namespaces` (`namespace_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `fk_schema_id` (`namespace_id`),
+  CONSTRAINT `fk_schema_id` FOREIGN KEY (`namespace_id`) REFERENCES `namespace` (`namespace_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -247,7 +241,7 @@ CREATE TABLE `namespaces_log` (
   `log_type` varchar(45) NOT NULL,
   KEY `fk_schema_idx` (`namespace_id`),
   KEY `fk_schema_crawl_idx` (`crawl_id`),
-  CONSTRAINT `fk_schema_log_key_id` FOREIGN KEY (`namespace_id`) REFERENCES `namespaces` (`namespace_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_schema_log_key_id` FOREIGN KEY (`namespace_id`) REFERENCES `namespace` (`namespace_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_schema_log_crawl_log_id` FOREIGN KEY (`crawl_id`) REFERENCES `crawl_log` (`crawl_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -269,7 +263,7 @@ CREATE TABLE `resource_instance_log` (
   KEY `crawl_id_UNIQUE` (`crawl_id`),
   CONSTRAINT `fk_crawl_resource_instance_log` FOREIGN KEY (`crawl_id`) REFERENCES `crawl_log` (`crawl_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_resource_instance_resource_instance_log` FOREIGN KEY (`resource_id`) REFERENCES `resource_instances` (`resource_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=182654 DEFAULT CHARSET=utf8 COMMENT='InnoDB free: 25600 kB; (`crawl_id`) REFER `ld_dataset_crawle';
+) ENGINE=InnoDB AUTO_INCREMENT=298860 DEFAULT CHARSET=utf8 COMMENT='InnoDB free: 25600 kB; (`crawl_id`) REFER `ld_dataset_crawle';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -326,7 +320,7 @@ CREATE TABLE `resource_instances` (
   KEY `fk_dataset_idx` (`dataset_id`),
   KEY `res_uri_idx` (`resource_uri`(255)),
   CONSTRAINT `fk_dataset_resource_instances` FOREIGN KEY (`dataset_id`) REFERENCES `dataset` (`dataset_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=168121 DEFAULT CHARSET=utf8 COMMENT='InnoDB free: 29696 kB; (`dataset_id`) REFER `ld_dataset_craw';
+) ENGINE=InnoDB AUTO_INCREMENT=284528 DEFAULT CHARSET=utf8 COMMENT='InnoDB free: 29696 kB; (`dataset_id`) REFER `ld_dataset_craw';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -345,7 +339,7 @@ CREATE TABLE `resource_type_log` (
   KEY `fk_rtl_crawl_id` (`crawl_id`),
   KEY `fk_rtl_schema_id` (`namespace_id`),
   KEY `log_type_idx` (`log_type`),
-  CONSTRAINT `fk_rtl_schema_id_key` FOREIGN KEY (`namespace_id`) REFERENCES `namespaces` (`namespace_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_rtl_schema_id_key` FOREIGN KEY (`namespace_id`) REFERENCES `namespace` (`namespace_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_rtl_crawl_id_key` FOREIGN KEY (`crawl_id`) REFERENCES `crawl_log` (`crawl_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_rtl_type_id_key` FOREIGN KEY (`type_id`) REFERENCES `resource_types` (`type_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -366,8 +360,8 @@ CREATE TABLE `resource_types` (
   PRIMARY KEY  (`type_id`),
   UNIQUE KEY `type_idx` (`type_uri`),
   KEY `fk_schema_idx` (`namespace_id`),
-  CONSTRAINT `fk_schema` FOREIGN KEY (`namespace_id`) REFERENCES `namespaces` (`namespace_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=350 DEFAULT CHARSET=utf8;
+  CONSTRAINT `fk_schema` FOREIGN KEY (`namespace_id`) REFERENCES `namespace` (`namespace_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=623 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -406,7 +400,7 @@ CREATE TABLE `resource_values` (
   KEY `resource_value_idx` (`resource_value_id`),
   KEY `prop_idx` (`property_uri`(255)),
   CONSTRAINT `fk_resource` FOREIGN KEY (`resource_id`) REFERENCES `resource_instances` (`resource_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=4364138 DEFAULT CHARSET=utf8 COMMENT='InnoDB free: 9216 kB; (`resource_id`) REFER `ld_dataset_craw';
+) ENGINE=InnoDB AUTO_INCREMENT=5712621 DEFAULT CHARSET=utf8 COMMENT='InnoDB free: 9216 kB; (`resource_id`) REFER `ld_dataset_craw';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -434,4 +428,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-06-26 10:49:28
+-- Dump completed on 2014-08-11 16:15:24
